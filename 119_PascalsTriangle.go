d6a6dfb6b688d8bf0f8cdc3db9d999ffb3b2f8e0
belokmaster <sychev.nickolay1@gmail.com>
2025-01-04 15:08:59 +0300

    add

---
+++ 119_PascalsTriangle.go
@@ -0,0 +1,27 @@
+func generate(numRows int) [][]int {
+	var answer [][]int
+	answer = append(answer, []int{1})
+	if numRows == 1 {
+		return answer
+	}
+
+	answer = append(answer, []int{1, 1})
+	if numRows == 2 {
+		return answer
+	}
+
+	for i := 2; i < numRows; i++ {
+		dig := make([]int, i+1)
+		dig[0] = 1
+		dig[i] = 1
+		for j := 1; j < len(dig)-1; j++ {
+			dig[j] = answer[i-1][j] + answer[i-1][j-1]
+		}
+		answer = append(answer, dig)
+	}
+	return answer
+}
+
+func getRow(rowIndex int) []int {
+	return generate(rowIndex + 1)[rowIndex]
+}
