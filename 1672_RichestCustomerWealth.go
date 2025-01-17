e772ec305970c48e2572dc7000ab7e1aefcb0a10
belokmaster <sychev.nickolay1@gmail.com>
2025-01-17 14:59:53 +0300

    easy tasks 17.01.2025

---
+++ 1672_RichestCustomerWealth.go
@@ -0,0 +1,15 @@
+package main
+
+func maximumWealth(accounts [][]int) int {
+	ans := 0
+	for i := 0; i < len(accounts); i++ {
+		sum := 0
+		for j := 0; j < len(accounts[i]); j++ {
+			sum += accounts[i][j]
+		}
+		if sum > ans {
+			ans = sum
+		}
+	}
+	return ans
+}
---
+++ 2942_FindWordsContainingCharacter.go
@@ -0,0 +1,34 @@
+package main
+
+func consistWord(s string, x byte) bool {
+	for _, w := range s {
+		if byte(w) == x {
+			return true
+		}
+	}
+	return false
+}
+
+func findWordsContaining(words []string, x byte) []int {
+	ans := []int{}
+	for i, w := range words {
+		if consistWord(w, x) {
+			ans = append(ans, i)
+		}
+	}
+	return ans
+}
+
+func findWordsContaining1(words []string, x byte) []int {
+	var res []int
+	for i, word := range words {
+		for _, c := range word {
+			if c == rune(x) {
+				res = append(res, i)
+				break
+			}
+		}
+	}
+
+	return res
+}
