3f95ecc38c0b545067f8ef7d1058e640a951a110
belokmaster <sychev.nickolay1@gmail.com>
2025-01-25 16:33:15 +0300

    fix

--- 43_MultiplyStrings.go
+++ 43_MultiplyStrings.go
@@ -9,7 +9,6 @@ import (
 */
 
 func multiply(num1 string, num2 string) string {
-	// Если одно из чисел равно "0", то результат умножения будет "0"
 	if num1 == "0" || num2 == "0" {
 		return "0"
 	}
