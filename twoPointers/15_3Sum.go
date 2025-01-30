8d7990ccf6dc5a12706be1e168e7f49483bfc119
belokmaster <sychev.nickolay1@gmail.com>
2025-01-30 13:43:01 +0300

    30.01.2025

---
+++ twoPointers/15_3Sum.go
@@ -0,0 +1,38 @@
+package main
+
+import "sort"
+
+func threeSum(nums []int) [][]int {
+	sort.Ints(nums)
+	ans := [][]int{}
+	n := len(nums)
+
+	for i := 0; i < n-2; i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+
+		left := i + 1
+		right := n - 1
+		for left < right {
+			sum := nums[i] + nums[left] + nums[right]
+			if sum == 0 {
+				ans = append(ans, []int{nums[i], nums[left], nums[right]})
+				for left < right && nums[left] == nums[left+1] {
+					left++
+				}
+				for left < right && nums[right] == nums[right-1] {
+					right--
+				}
+				left++
+				right--
+			} else if sum < 0 {
+				left++
+			} else {
+				right--
+			}
+		}
+	}
+
+	return ans
+}
